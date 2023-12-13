d776137f95b686b2b7b23dbc2bfbe21025da0db4
Ante Bucan <ab8390@g.rit.edu>
2023-12-13 20:49:05 +0100

    .

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        data={days}
+        renderItem={({ item }) => {
+          return <DayListItem day={item} />;
+        }}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,7 +25,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    gap: 10,
+  },
+  column: {
+    gap: 10,
+    padding: 10,
   },
 });
